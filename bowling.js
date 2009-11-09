951a64744e7e2cd23865afb05076f4d564c82ba3
Carlos Villela <cv@lixo.org>
2009-11-08 22:43:43 -0200

    final outcome of today's coding dojo

--- bowling.js
+++ bowling.js
@@ -1,3 +1,4 @@
+/*
 SomeObject = function() {
   var privateMember = "some value";
   var privateMethod = function() {
@@ -13,4 +14,83 @@ SomeObject = function() {
       return privateMember + " " +  parameter;
     }
   }
+}
+*/
+BowlingFrame = function(score1, score2) {
+
+  
+   return {
+     score1: score1,
+     score2: score2,
+     
+     score: function(){
+       /*if (this.isSpare()) {
+         return undefined;
+       }*/
+       return score1 + score2;
+     },
+     
+     getFirstThrow: function(){
+       return score1;
+     },
+     
+     isSpare: function() {
+       return this.score1 + this.score2 === 10;
+     }
+   }  
+  
+}
+
+  BowlingGame = function() {
+
+  
+  return {
+    frames:new Array(),
+    
+    
+    addFrame: function(score1, score2) {
+      var f = BowlingFrame(score1, score2);
+      this.frames.push(f);    
+    
+    },
+    
+    frameScore: function(index) {
+      var totalScore = 0;
+      var nextFrame = index + 1 ;
+ 
+      if (this.frames[this.frames.length-1].score() === undefined) {
+        return undefined;
+      }
+      for (var i = 0; i < (nextFrame || this.frames.length); i++) {
+        
+        if(i > 0 && this.frames[i-1].isSpare()){
+          totalScore += this.frames[i].getFirstThrow();
+        }
+        
+        if(i != index) {
+          totalScore += this.frames[i].score();
+        }
+      }
+      
+      
+      
+      
+      return totalScore;
+    },
+    score: function() {
+      var totalScore = 0;
+      
+      if (this.frames[this.frames.length-1].isSpare()) {
+        return undefined;
+      }
+      for (var i = 0; i < this.frames.length; i++) {
+        totalScore += this.frames[i].score();
+      }
+      
+      
+      
+      return totalScore;
+    }
+    
+  };
 }
--- bowling_test.js
+++ bowling_test.js
@@ -11,25 +11,72 @@
     "The lazy brown fox jumped the log.",
     "Compare a string using a RegExp."
   );
+
+"should pass" : function(assert) {
+  assert.ok(typeof 1 === "number", "1 is a number");
+},
+
+"should fail" : function(assert) {
+  assert.ok(typeof 1 === "string", "1 is not a string");
+},
+
+"SomeObject" : {
+  "should have access and visibility controls" : function(assert) {
+    var obj = new SomeObject();
+    assert.compare(obj.publicMethod(), "some value", "should have access to public methods");
+    assert.compare(obj.anotherPublicMethod(3), "some value 3", "public methods should have access to private members");
+    assert.compare(typeof obj["privateMember"], "undefined", "private members should not be accessible");
+    assert.compare(typeof obj["privateMethod"], "undefined", "private methods should not be accessible");
+  }
+}
+
+
 */
 
 var tests = {
 
-  "should pass" : function(assert) {
-    assert.ok(typeof 1 === "number", "1 is a number");
+  "should score one frame": function(assert) {
+    var b = new BowlingGame();
+    b.addFrame(1, 4);
+    assert.ok(5 == b.score(), "isScoreOK");
+    b.addFrame(4, 5)
+    assert.ok(14 == b.score(), "secodScoreOK")
+    b.addFrame(6, 4);
+    assert.ok(undefined == b.score(), "thirdScoreUndefinedOk");
+    b.addFrame(5, 5);
+    assert.ok(29 == b.frameScore(2), "thirdScoreAfterSpareOk");
+    assert.ok(undefined == b.score(), "fourthScoreUndefinedOk");
+    //b.addFrame(10, 0);
+    //assert.ok(29 == b.score(), "thirdScoreAfterSpareOk");
   },
+  
+  "should return if frame is spare" : function(assert) {
+    var f = new BowlingFrame(3, 4);
+    assert.ok(!f.isSpare(), "notSpareOK");
 
-  "should fail" : function(assert) {
-    assert.ok(typeof 1 === "string", "1 is not a string");
+    f = new BowlingFrame(4, 6);
+    assert.ok(f.isSpare(), "spareOK");
+    
+    f = new BowlingFrame(10, 0);
+    assert.ok(f.isSpare(), "strikeIsSpareForNow");
   },
-
-  "SomeObject" : {
-    "should have access and visibility controls" : function(assert) {
-      var obj = new SomeObject();
-      assert.compare(obj.publicMethod(), "some value", "should have access to public methods");
-      assert.compare(obj.anotherPublicMethod(3), "some value 3", "public methods should have access to private members");
-      assert.compare(typeof obj["privateMember"], "undefined", "private members should not be accessible");
-      assert.compare(typeof obj["privateMethod"], "undefined", "private methods should not be accessible");
-    }
+  
+  /*
+  "should add and score one Frame Ojbect": function(assert){
+    var b = new BowlingGame();
+    b.addFrame(1,4);
+    assert.ok(5 == b.getFrame(0).score(), "isFrameScore")
+  },*/
+  "should score one Frame Ojbect": function(assert){
+    var f = new BowlingFrame(1,4);
+    
+    assert.ok(5 == f.score(), "isFrameScore");
+    var f = new BowlingFrame(5, 5);
+    //assert.ok(undefined === f.score(), "scoreUndefined");    
+  },
+  
+  "should return the first throw score": function(assert){
+    var f = new BowlingFrame(5,5);
+    assert.ok(f.getFirstThrow() === 5, "getFirstThrowOk")
   }
 }
